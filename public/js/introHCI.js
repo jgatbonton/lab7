6f37037a8ad535a922f012da4dde355ec2276d8b
Judd Gatbonton <judd.gatbonton@gmail.com>
2016-02-17 05:06:42 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$("#likeBtn").click(projectClick);
+}
+
+function projectClick(e) {
+	ga("send", "event", "like", "click");
 }
